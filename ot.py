1d492c15196ca66581efa0b27a1a17a479b05d60
mkl <mkl@ableton.com>
2014-03-21 10:23:37 +0100

    Revert conflicting adds

--- ot.py
+++ ot.py
@@ -1,3 +1,18 @@
+def xform(operation1, operation2):
+    return operation2.reverted(), operation1.reverted()
+
+class Remove(object):
+    def __init__(self, position, value):
+        self.position = position
+        self.value = value
+
+    def apply(self, data):
+        assert data[self.position] == self.value
+        data.pop(self.position)
+
+    def reverted(self):
+        return Add(self.position, self.value)
+
 class Add(object):
     def __init__(self, position, value):
         self.position = position
@@ -6,21 +21,33 @@ class Add(object):
     def apply(self, data):
         data.insert(self.position, self.value)
 
+    def reverted(self):
+        return Remove(self.position, self.value)
+
 class Client(object):
     def __init__(self):
         self.data = []
         self.remotes = []
+        self.num_sent_messages = 0
+        self.num_received_messages = 0
+        self.sent_messages = []
 
     def generate(self, operation):
         operation.apply(self.data)
         self.send(operation)
+        self.sent_messages.append(operation)
+        self.num_sent_messages += 1
 
     def add_remote(self, remote):
         self.remotes.append(remote)
 
     def send(self, operation):
         for remote in self.remotes:
-            remote.receive(operation)
+            remote.receive(operation, self.num_sent_messages, self.num_received_messages)
 
-    def receive(self, operation):
+    def receive(self, operation, num_sent_messages, num_received_messages):
+        assert self.num_received_messages == num_sent_messages
+        for i, message in enumerate(self.sent_messages):
+            operation, self.sent_messages[i] = xform(operation, message)
         operation.apply(self.data)
+        self.num_received_messages += 1
--- test_ot.py
+++ test_ot.py
@@ -1,6 +1,18 @@
 import unittest
 from ot import Add, Client
 
+class CaptureCallToReceive(object):
+    def __init__(self, remote):
+        self.remote = remote
+        self.args = None
+
+    def receive(self, *args):
+        assert self.args == None
+        self.args = args
+
+    def call(self):
+        self.remote.receive(*self.args)
+
 class TestTransform(unittest.TestCase):
     def test_generating_operation_changes_data(self):
         client = Client()
@@ -17,3 +29,24 @@ class TestTransform(unittest.TestCase):
         self.assertEquals(client1.data, ['x'])
         self.assertEquals(client2.data, ['x'])
 
+    def test_conflicting_adds_are_reverted(self):
+        client1 = Client()
+        client2 = Client()
+
+        remote1 = CaptureCallToReceive(client1)
+        remote2 = CaptureCallToReceive(client2)
+
+        client1.add_remote(remote2)
+        client2.add_remote(remote1)
+
+        client1.generate(Add(0, 'x'))
+        client2.generate(Add(0, 'y'))
+
+        self.assertEquals(client1.data, ['x'])
+        self.assertEquals(client2.data, ['y'])
+
+        remote1.call()
+        remote2.call()
+
+        self.assertEquals(client1.data, [])
+        self.assertEquals(client2.data, [])
